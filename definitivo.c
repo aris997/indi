2aab7f092e67df7bf81e6b3139e05c5e315e91d7
aris997 <riccardo.riva.997@gmail.com>
2017-10-29 18:12:16 +0100

    gfd

--- definitivo.c
+++ definitivo.c
@@ -25,6 +25,8 @@ double f(double, double, double, pars);
 double g(double, double, double, pars);
 double h(double, double, double, pars);
 
+double ask(char* argomento[], double, double);
+
 
 int main(int argc, char *argv[]) {
 
@@ -32,7 +34,7 @@ int main(int argc, char *argv[]) {
 	long int i, steps, Hstep;
 	double x0, y0, z0;
   double C, Co, tmax, delta_periodo;
-  //char filename[30];
+  char filename[30];
 	
   double *dperiodo;
 
@@ -42,7 +44,7 @@ int main(int argc, char *argv[]) {
 	FILE *cost;
   FILE *costerr;
 	//FILE *piripicchio;
-  //FILE *rho_variable;
+  FILE *rho_variable;
   //FILE *drastico;
   //FILE *gpscript;
 	
@@ -51,36 +53,15 @@ int main(int argc, char *argv[]) {
   vector e;
 	pars c;
 
-  if ( argc == 10 ) {
-    c.a = atof(argv[2]);
-    c.b = atof(argv[3]);
-    c.rho = atof(argv[4]);
-    p.x = atof(argv[5]);
-    p.y = atof(argv[6]);
-    p.z = atof(argv[7]);
-    c.dt = atof(argv[8]);
-    tmax = atof(argv[9]);
-  
-    if (tmax < 50. && c.dt >= 1.) {
-      printf("#tmax troppo piccolo o dt troppo grande\nEsecuzione invalidata\n");
-      exit(-1);
-    }
-  }
+    c.a = ask("a", 0, 10);
+    c.b = ask("b", 0, 10);
+    c.rho = ask("rho", 0, 5);
+    p.x = ask("x0", 0, 10);
+    p.y = ask("y0", 0, 10);
+    p.z = ask("z0", 0, 10);
+    c.dt = ask("dt", 0.0001, 1.);
+    tmax = ask("tmax", 50., 20000.);
 
-
-
-  else {
-    printf("#Per valori a piacere inserire da shell dopo il richiamo: a b rho x0 y0 z0 dt tmax\n");
-    printf("#l esecuzione prenderà i dati di default\n");
-    p.x = 1.2;
-    p.y = 0.5;
-    p.z = 0.;
-    c.a = 0;
-    c.b = 0;
-    c.rho = 0.2;
-    c.dt = 0.01;
-    tmax = 1000.;
-  }
 	
 	output = fopen("output.dat", "w");
 	fprintf(output, "#t\t\t\tx\t\t\t\t\ty\t\t\t\t\tz\n");
@@ -148,8 +129,10 @@ int main(int argc, char *argv[]) {
 
   printf("#Vario dt per osservare l'andamento di C/Co - 1\n");
   
+
+
   /****ricalcolo il moto fino a 10.t variando il dt****/
-  for (c.dt=1.; c.dt>=0.0001; c.dt-=0.00001) {
+  for (c.dt=1.; c.dt>=0.0001; c.dt/=10.) {
     tmax = 10.;
     steps = (long int)(tmax/c.dt);
     p.x = x0;
@@ -165,23 +148,46 @@ int main(int argc, char *argv[]) {
       /****************************/
       /****************************/
       /****************************/
-      /****INIZIO SECONDA PARTE****/printf("#INIZIO SECONDA PARTE\n");
+      /****INIZIO SECONDA PARTE****/printf("#Parte 2\n");
       /****************************/
       /****************************/
       /****************************/
-/*
-	for (j=0; j<2; j++) {
 
-    //printf("#E %d\n", j+1);
+  printf("#Se rho massimo e minimo sono uguali si eseguira un solo ciclo\n");
+
+
+
+  double Mrho = ask("rho massimo", 0, 5);
+  double mrho = ask("rho minimo", 0, Mrho);
+  double drho, passo;
+  double dperiodototale;
+  int w;
+
+  if (Mrho != mrho ) {
+    passo = ask("passo, non troppo piccolo per intervalli grandi", 0.001, fabs(Mrho-mrho));
+    drho = fabs(Mrho-mrho)*passo;
+  }
+
+
+	for (c.rho=mrho; c.rho<Mrho; c.rho+=drho) {
+
 
     dperiodo = (double *) malloc(NUM_PERIODI * sizeof(double));
     if (dperiodo == NULL ) { printf("malloc error\n"); exit(-1); }
     dperiodo = (double *) calloc(NUM_PERIODI, sizeof(double));
 
-		//fscanf(input, "%lf %lf %lf %lf %lf %lf %lf %lf\n", &c.a, &c.b, &c.rho, &x0, &y0, &z0, &c.dt, &tmax);
 
-		sprintf(filename, "rho%0*d.dat", width, (int)(c.rho*100.));
-		rho_variable = fopen(filename, "w");
+
+    c.a = 0.5;
+    c.b = 0.1;
+    x0 = 1.2;
+    y0 = 0.5;
+    z0 = 0.3;
+    tmax = 10000.;
+
+
+		//sprintf(filename, "rho%0*d.dat", width, (int)(c.rho*100.));
+		//rho_variable = fopen(filename, "w");
 
 		p.x = x0; p.y = y0; p.z = z0;
     k = 0; l = 0;
@@ -189,10 +195,10 @@ int main(int argc, char *argv[]) {
     steps = (long int)(tmax/c.dt);
     Hstep = steps/2;
 
-    sprintf(filename, "periodorho%0*d.dat", width, (int)(c.rho*100.));
-    periodo = fopen(filename, "w");
-    fprintf(periodo, "#Condizioni Iniziali\n");
-    fprintf(periodo, "#%lf %lf %lf %lf %lf %lf %lf %lf\n", p.x, p.y, p.z, c.a, c.b, c.rho, c.dt, tmax);
+    //sprintf(filename, "periodorho%0*d.dat", width, (int)(c.rho*100.));
+    //periodo = fopen(filename, "w");
+    //fprintf(periodo, "#Condizioni Iniziali\n");
+    //fprintf(periodo, "#%lf %lf %lf %lf %lf %lf %lf %lf\n", p.x, p.y, p.z, c.a, c.b, c.rho, c.dt, tmax);
 
 		for (i=0; i <= steps; i++) {
 
@@ -206,16 +212,15 @@ int main(int argc, char *argv[]) {
       }
 			
       else if ( i > Hstep/2. ) { 
-        fprintf(rho_variable, "%.4lf %.8lf %.8lf %.8lf\n", c.dt*((double)(i+1)), p.x, p.y, p.z);
+        //fprintf(rho_variable, "%.4lf %.8lf %.8lf %.8lf\n", c.dt*((double)(i+1)), p.x, p.y, p.z);
 
         // controllo se il moto del punto si trova nei punti scelti S e Sold 
         if (e.x >= fabs(p.x - S.x) && e.y >= fabs(p.y - S.y) && e.z >= fabs(p.z - S.z) ) {
-
           //controllo se la direzione del passaggio è la stessa
           if (e.x >= fabs(pold.x - Sold.x) && e.y >= fabs(pold.y - Sold.y) && e.z >= fabs(pold.z - Sold.z) ) {
             
             dperiodo[k] = (double)i*c.dt;
-            k++; l++; //aggiornamneto contatori
+            k++; //aggiornamneto contatori
 
             if (k == (kstop - 4) ) { //
               dperiodo = (double *) realloc( dperiodo, 2 * kstop * sizeof(double)); 
@@ -223,116 +228,43 @@ int main(int argc, char *argv[]) {
               printf("#reallocated dperiod memory\n"); 
             }
             
-            if (k > 1) {
-              fprintf(periodo, "%.8lf %.14lf\n", (double)i*c.dt, dperiodo[k-1]-dperiodo[k-2]); 
-            }
-            
-            // if ( k > 2 && dperiodo[k-1]-dperiodo[k-2] < (dperiodo[k-2]-dperiodo[k-3])/4.) {
-            //   printf("#A S I N T O T O   T I M E\t %lf\n",( ( (double)i*c.dt) - 3 ) );
-            //   fprintf(rho_variable, "#Asintotico a {x:%.8lf, y:%.8lf, z:%.8lf, t:%lf}\n", p.x, p.y, p.z, (double)i*c.dt);
-            //   i = steps;
-            // }
+            //if (k > 1) {
+              //fprintf(periodo, "%.8lf %.14lf\n", (double)i*c.dt, dperiodo[k-1]-dperiodo[k-2]); 
+            //}
           }
         }
       }
     }
-
+    if (k > 0) {
+      for (w=0; w<k; w++) dperiodototale += dperiodo[w];
+      dperiodototale/=k;
+      printf("#periodo rho: %lf \tT %.8lf \tincontri %d\n", c.rho, dperiodototale, k+1);
+    }
+    else {
+      printf("#periodo NON esiste per rho: %lf", c.rho);
+    }
 
 
     free(dperiodo);
-    fclose(rho_variable);
-    fclose(periodo);
+    //fclose(rho_variable);
+    //fclose(periodo);
 
 	}
 
-*/
-  printf("sasd\n");
 
-/****RIORDINARE QUESTA SEZIONE (RIGUARDA LA PARTE 2)****/
-/*
-	c.a = 0.5;
-	c.b = 0.1;
-	x0 = 1.2;
-	y0 = 0.5;
-	z0 = 0.3;
-	c.dt = 0.01;
-	tmax = 200;
-
-	steps = (long int)(tmax/c.dt);
-  Hstep = steps/2;
-
-
-	for (j=0; j<=50; j++)  { //100 passi per arrivare da 1.2 a 1.5 rho
-
-
-		c.rho = 1.200;
-		c.rho += 0.006*(double)j; //incremento dell'1% dell'intervallo
-		p.x = x0;
-		p.y = y0;
-		p.z = z0;
-    k = 0;
-
-    
-    dperiodo = (double *) malloc(NUM_PERIODI * sizeof(double));
-    if (dperiodo == NULL ) { printf("malloc error\n"); exit(-1); }
-    dperiodo = (double *) calloc(NUM_PERIODI, sizeof(double));
-    kstop = NUM_PERIODI;
-		
-
-    for (i = 0; i<=steps; i++) { //steps dovrebbe essere 8000
-
-			pold = strcopy(p);
-			p = RK4(p, c);
-
-			if (i == Hstep) {
-        e = intorno(S, Sold, 20);
-        if (e.x >= fabs(p.x - S.x) && e.y >= fabs(p.y - S.y) && e.z >= fabs(p.z - S.z) ) {
-          
-          if (e.x >= fabs(pold.x - Sold.x) && e.y >= fabs(pold.y - Sold.y) && e.z >= fabs(pold.z - Sold.z) ) {
-            //printf("#cambiamento non drastico con rho:%.8lf\n", c.rho);
-          }
-        }
-        else {
-          //printf("#cambiamento drastico, con rho:%.8lf\n", c.rho);
-        }      
-
-
-        Sold = strcopy(pold);
-        S = strcopy(p);
-        e = intorno(S, Sold, 0.7);
-      }
-			
-      if (i > Hstep) {
-
-        //printf("%lf %lf %lf ", p.x, p.y, p.z);
-				
-        if (e.x >= fabs(p.x - S.x) && e.y >= fabs(p.y - S.y) && e.z >= fabs(p.z - S.z) ) {
-          if (e.x >= fabs(pold.x - Sold.x) && e.y >= fabs(pold.y - Sold.y) && e.z >= fabs(pold.z - Sold.z) ) {
-            //printf("#periodo %lf\n", (double)i*c.dt); 
-            dperiodo[k] = (double)i*c.dt; k++;
-        
-            //if (k > 1) printf("%lf %.8lf ", c.rho, dperiodo[k-1]-dperiodo[k-2]);
-           
-            if (k == (kstop - 4) ) { //
-              dperiodo = (double *) realloc( dperiodo, 2 * kstop * sizeof(double)); 
-              kstop *= 2; 
-              //printf("#reallocated dperiod memory\n"); 
-            }
-          }
-				}
-        //printf("\n");
-			}
-		}
-  free(dperiodo);
-	}
-*/
-  printf("porcodio");
-  
   fclose(costerr);
-	//fclose(input);
-
+	
 	exit(0);
-}//////////////////////////////// end main
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -400,3 +332,19 @@ struct vector intorno(vector S, vector Sold, double i) {
 
   return e;
 }
+
+double ask(char* argomento[], double a, double b) {
+  double w;
+
+  printf("#Inserire %s\n", argomento);
+
+  do {
+    scanf("%lf", &w);
+    if ( w < a || w > b ) {
+      printf("#Valore errato, %s assumere valori [%.2lf:%.2lf]\n", argomento, a, b);
+    }
+  } while ( w < a || w > b );
+
+  return w;
+}
+
